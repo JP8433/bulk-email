bfca4e0ed06cc9f138e2dba89d9b4975e2f22fcb
Jay Patel <68432630+JP8433@users.noreply.github.com>
2023-07-20 21:22:34 +0530

    Update url.js

--- Frontend/src/url.js
+++ Frontend/src/url.js
@@ -1,7 +1,7 @@
 
 <<<<<<< HEAD
- //const API = "http://localhost:4000"
-// =======
+ const API = "http://localhost:4000"
+ =======
  const API = "https://backend-service5.onrender.com"
 >>>>>>> 1a4f379ec711f2c485e55fe58ee1d4b2834a24f7
 
