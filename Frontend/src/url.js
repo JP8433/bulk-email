bf60d63afa2445e228b05f491b9205b1f4dc4caa
Jay Patel <68432630+JP8433@users.noreply.github.com>
2023-07-20 21:14:43 +0530

    Update url.js

--- Frontend/src/url.js
+++ Frontend/src/url.js
@@ -1,6 +1,6 @@
-//  const API = "https://vercel.app" //
+
 <<<<<<< HEAD
- const API = "http://localhost:4000"
+ //const API = "http://localhost:4000"
 =======
  const API = "https://backend-service5.onrender.com"
 >>>>>>> 1a4f379ec711f2c485e55fe58ee1d4b2834a24f7
